a75039e7892dbe10a771cd27d4c72de6949ec44c
Bhuvan-S-prasad <bhuvansprasad20@gmail.com>
2025-12-08 22:15:00 +0530

    feat: add Leftbar component with navigation links and logout functionality, along with its corresponding constant data.

--- components/shared/Leftbar.tsx
+++ components/shared/Leftbar.tsx
@@ -1,8 +1,47 @@
+'use client'
+
+import { sidebarLinks } from "@/constants";
+import { SignedIn, SignOutButton } from "@clerk/nextjs";
+import { LogOut } from "lucide-react";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+
 function Leftbar() {
+    const pathname = usePathname();
+    const router = useRouter();
+
     return (
-        <div>
-            Leftbar
-        </div>
+        <section className="custom-scrollbar leftsidebar">
+            <div className="flex w-full flex-1 flex-col gap-6 px-6">
+                {sidebarLinks.map((link) => {
+                    const isActive = (pathname.includes(link.route) && link.route.length > 1) || pathname === link.route;
+
+                    return (
+                        <div>
+                            <Link href={link.route}
+                                key={link.label}
+                                className={`leftsidebar_link ${isActive && 'bg-primary-500'}`}>
+                                <span className="w-6 h-6">{link.icon}</span>
+                                <p className="text-light-1 max-xs:hidden">{link.label}</p>
+                            </Link >
+                        </div >
+                    )
+                }
+                )}
+
+            </div >
+
+            <div className="mt-10 px-6">
+                <SignedIn>
+                    <SignOutButton signOutCallback={() => router.push('/sign-in')}>
+                        <div className="flex cursor-pointer gap-4 p-4">
+                            <LogOut />
+                            <p className="text-light-1 max-xs:hidden">Logout</p>
+                        </div>
+                    </SignOutButton>
+                </SignedIn>
+            </div>
+        </section >
     )
 }
 
